31c4334c2d70435c1205ab86b488f038664813dc
Awa Gaye <freva07@gmail.com>
2021-09-29 10:31:21 +0200

    Bonjour

---
+++ serie1jqery.js
@@ -0,0 +1,29 @@
+
+
+
+//$(window).load(function(){
+$(".btn1").addClass("btn btn-primary btn-lg").click(function(){                     // creation de text
+    $('#msgid3').text("This is hello world by javascript");})
+
+
+   
+
+    $("#mami").html("bonjour tout le monde");
+    $('#titi').html("je meurs d'envie de partir en vacances");
+
+
+   
+    $("#hide").click(function(){
+      $('#titi').hide();
+    });
+
+
+    $("body").css("background-color", "red");
+
+
+   // $('#momo').click(function(){
+      //  $('input').hide();
+    //});
+
+
+
